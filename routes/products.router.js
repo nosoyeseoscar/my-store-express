829f9f0e0ca6a2de3ac00eac04ccb407c81586b2
Oscar Fuentes <ofuentese@gmail.com>
2023-01-10 15:04:04 -0700

    adding services

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,37 +1,47 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const ProductService = require('./../services/product.service')
 
 const router = express.Router()
+const service = new ProductService()
 
 router.get('/',(req, res)=>{
-  const { size} = req.query
-  const limit = size || 10
-  const products = []
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.product(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    })
+  const products = service.find()
 
-  }
   res.json(products)
 })
 
 router.get('/:id', ( req,res)=>{
   const {id} = req.params;
-  res.json({
-    id,
-    name: 'producto 1',
-    price: 100,
-  })
+  const product = service.findOne(id)
+  res.json(product)
+
 })
 
 router.post('/', (req, res)=>{
   const body = req.body
-  res.json({
+  res.status(201).json({
     message: 'created product',
     data: body
+  })
+})
+
+router.patch('/:id', (req, res)=>{
+  const {id} = req.params
+  const body = req.body
+
+  res.json({
+    message: "update",
+    data: body,
+    id,
+  })
+})
+
+router.delete('/:id', (req, res)=>{
+  const {id} = req.params
+
+  res.json({
+    message: "deleted",
+    id,
   })
 })
 
