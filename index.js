b4fa752504b354f43dfa532455378d9313877fc3
Oscar Fuentes <ofuentese@gmail.com>
2023-01-12 14:22:57 -0700

    FEATURE: adding CORS configuration and implementations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes')
+const cors = require('cors')
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middleware/error.handler')
 
@@ -7,6 +8,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json())
+
+//whitelist de destinos CORS
+const whitelist = ['http://localhost:8080']
+const options = {
+  origin: (origin, callback)=>{
+    if (whitelist.includes(origin))
+      callback(null, true)
+    else
+      callback(new Error('Not allowed'))
+  }
+}
+
+app.use(cors(options));
 
 app.get('/',(req, res)=>{
   res.send('Hola mi server en express')
