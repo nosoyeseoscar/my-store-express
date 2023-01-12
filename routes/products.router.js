5e219441defb4f862a86658717616e5a1648efb6
Oscar Fuentes <ofuentese@gmail.com>
2023-01-12 10:42:15 -0700

    adding httperrors middleware

--- routes/products.router.js
+++ routes/products.router.js
@@ -10,10 +10,14 @@ router.get('/',(req, res)=>{
   res.json(products)
 })
 
-router.get('/:id', async ( req,res)=>{
-  const {id} = req.params;
-  const product = await service.findOne(id)
-  res.json(product)
+router.get('/:id', async ( req,res,next)=>{
+  try {
+    const {id} = req.params;
+    const product = await service.findOne(id)
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
 
 })
 
